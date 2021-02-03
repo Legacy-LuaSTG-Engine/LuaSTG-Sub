276611493c8be37ec3cd87a80fb473a57cb1d7e5
Xiliusha <Xiliusha@outlook.com>
2021-02-03 17:17:51 +0800

    彻底移除老的D3DX9的保存纹理代码

--- fancy2d/RenderDev/f2dRenderDeviceAPI.cpp
+++ fancy2d/RenderDev/f2dRenderDeviceAPI.cpp
@@ -4,7 +4,6 @@
 
 f2dRenderDeviceAPI::f2dRenderDeviceAPI() :
 	m_hModuleD3DX9(NULL),
-	m_Entry_pD3DXSaveSurfaceToFileInMemory(NULL),
 	m_Entry_pD3DXCreateEffectEx(NULL)
 {
 	// 加载模块 D3DX9
@@ -15,10 +14,6 @@ f2dRenderDeviceAPI::f2dRenderDeviceAPI() :
 	
 	// 获得入口 D3DX9
 	
-	m_Entry_pD3DXSaveSurfaceToFileInMemory = (pDLLEntry_D3DXSaveSurfaceToFileInMemory)GetProcAddress(m_hModuleD3DX9, "D3DXSaveSurfaceToFileInMemory");
-	if(!m_Entry_pD3DXSaveSurfaceToFileInMemory)
-		throw fcyWin32Exception("f2dRenderDeviceAPI::f2dRenderDeviceAPI", "GetProcAddress(D3DXSaveSurfaceToFileInMemory) Failed.");
-	
 	m_Entry_pD3DXCreateEffectEx = (pDLLEntry_D3DXCreateEffectEx)GetProcAddress(m_hModuleD3DX9, "D3DXCreateEffectEx");
 	if(!m_Entry_pD3DXCreateEffectEx)
 		throw fcyWin32Exception("f2dRenderDeviceAPI::f2dRenderDeviceAPI", "GetProcAddress(D3DXCreateEffectEx) Failed.");
@@ -26,16 +21,6 @@ f2dRenderDeviceAPI::f2dRenderDeviceAPI() :
 
 f2dRenderDeviceAPI::~f2dRenderDeviceAPI()
 {
-}
-
-HRESULT f2dRenderDeviceAPI::DLLEntry_D3DXSaveSurfaceToFileInMemory(
-	LPD3DXBUFFER*             ppDestBuf,
-	D3DXIMAGE_FILEFORMAT      DestFormat,
-	LPDIRECT3DSURFACE9        pSrcSurface,
-	CONST PALETTEENTRY*       pSrcPalette,
-	CONST RECT*               pSrcRect)
-{
-	return m_Entry_pD3DXSaveSurfaceToFileInMemory(ppDestBuf, DestFormat, pSrcSurface, pSrcPalette, pSrcRect);
 }
 
 HRESULT f2dRenderDeviceAPI::DLLEntry_D3DXCreateEffectEx(
--- fancy2d/RenderDev/f2dRenderDeviceAPI.h
+++ fancy2d/RenderDev/f2dRenderDeviceAPI.h
@@ -17,12 +17,6 @@
 class f2dRenderDeviceAPI
 {
 private:
-    typedef HRESULT (WINAPI *pDLLEntry_D3DXSaveSurfaceToFileInMemory)(
-        LPD3DXBUFFER*             ppDestBuf,
-        D3DXIMAGE_FILEFORMAT      DestFormat,
-        LPDIRECT3DSURFACE9        pSrcSurface,
-        CONST PALETTEENTRY*       pSrcPalette,
-        CONST RECT*               pSrcRect);
     typedef HRESULT (WINAPI *pDLLEntry_D3DXCreateEffectEx)(
         LPDIRECT3DDEVICE9               pDevice,
         LPCVOID                         pSrcData,
@@ -36,15 +30,8 @@ private:
         LPD3DXBUFFER*                   ppCompilationErrors);
 private:
 	HMODULE m_hModuleD3DX9;
-	pDLLEntry_D3DXSaveSurfaceToFileInMemory m_Entry_pD3DXSaveSurfaceToFileInMemory;
 	pDLLEntry_D3DXCreateEffectEx m_Entry_pD3DXCreateEffectEx;
 public:
-    HRESULT DLLEntry_D3DXSaveSurfaceToFileInMemory(
-        LPD3DXBUFFER*             ppDestBuf,
-        D3DXIMAGE_FILEFORMAT      DestFormat,
-        LPDIRECT3DSURFACE9        pSrcSurface,
-        CONST PALETTEENTRY*       pSrcPalette,
-        CONST RECT*               pSrcRect);
     HRESULT DLLEntry_D3DXCreateEffectEx(
         LPDIRECT3DDEVICE9               pDevice,
         LPCVOID                         pSrcData,
