58161610d08a3d4f6dabeb4b87aed62e55956823
Kuanlan <Kuanlan@outlook.com>
2021-11-27 20:42:36 +0800

    so fxxk you

--- slow/include/slow/Object.hpp
+++ slow/include/slow/Object.hpp
@@ -4,66 +4,66 @@
 // reference object
 namespace slow
 {
-	struct IObject
-	{
-		virtual i32 reserve() = 0;
-		virtual i32 release() = 0;
-		virtual ~IObject() {}
-	};
-
-	template<typename T>
-	class Object : public T
-	{
-	private:
-		volatile i32 _count;
-	public:
-		i32 reserve()
-		{
-			_count += 1;
-			return _count;
-		}
-		i32 release()
-		{
-			_count -= 1;
-			if (_count < 1)
-			{
-				const i32 temp_ = _count;
-				delete this;
-				return temp_;
-			}
-			return _count;
-		}
-	public:
-		Object() : _count(1) {}
-		virtual ~Object() {}
-	};
-
-	template<typename T = IObject>
-	class object_ptr
-	{
-	private:
-		T* _p;
-		inline void _reserve() { if (_p) { _p->reserve(); } }
-		inline void _release() { if (_p) { _p->release(); } _p = nullptr; }
-	public:
-		T* operator->() const { return _p; }
-		T* get() const { return _p; }
-		T** getAddress() { return &_p; }
-		object_ptr& operator=(T* p) { _release(); _p = p; _reserve(); return *this; }
-		object_ptr& setDirect(T* p) { _release(); _p = p; return *this; }
-		void reset() { _release(); }
-		T* getOwnership() { T* p = _p; _p = nullptr; return p; }
-		T** getNewAddress() { _release(); return &_p; }
-		T** operator~() { _release(); return &_p; }
-		operator bool() const { return _p != nullptr; }
-		operator T* () const { return _p; }
-	public:
-		object_ptr() : _p(nullptr) {}
-		object_ptr(T* p) : _p(p) { _reserve(); }
-		object_ptr(object_ptr& r) : _p(r._p) { _reserve(); }
-		object_ptr(const object_ptr& r) : _p(r._p) { _reserve(); }
-		object_ptr(object_ptr&& r) : _p(r._p) { r._p = nullptr; }
-		object_ptr(const object_ptr&&) = delete;
-		~object_ptr() { _release(); }
-	};
+    struct IObject
+    {
+        virtual i32 reserve() = 0;
+        virtual i32 release() = 0;
+        virtual ~IObject() {}
+    };
+    
+    template<typename T>
+    class Object : public T
+    {
+    private:
+        volatile i32 _count;
+    public:
+        i32 reserve()
+        {
+            _count += 1;
+            return _count;
+        }
+        i32 release()
+        {
+            _count -= 1;
+            if (_count < 1)
+            {
+                const i32 temp_ = _count;
+                delete this;
+                return temp_;
+            }
+            return _count;
+        }
+    public:
+        Object() : _count(1) {}
+        virtual ~Object() {}
+    };
+    
+    template<typename T = IObject>
+    class object_ptr
+    {
+    private:
+        T* _p;
+        inline void _reserve() { if (_p) { _p->reserve(); } }
+        inline void _release() { if (_p) { _p->release(); } _p = nullptr; }
+    public:
+        T* operator->() const { return _p; }
+        T* get() const { return _p; }
+        T** getAddress() { return &_p; }
+        object_ptr& operator=(T* p) { _release(); _p = p; _reserve(); return *this; }
+        object_ptr& setDirect(T* p) { _release(); _p = p; return *this; }
+        void reset() { _release(); }
+        T* getOwnership() { T* p = _p; _p = nullptr; return p; }
+        T** getNewAddress() { _release(); return &_p; }
+        T** operator~() { _release(); return &_p; }
+        operator bool() const { return _p != nullptr; }
+        operator T* () const { return _p; }
+    public:
+        object_ptr() : _p(nullptr) {}
+        object_ptr(T* p) : _p(p) { _reserve(); }
+        object_ptr(object_ptr& r) : _p(r._p) { _reserve(); }
+        object_ptr(const object_ptr& r) : _p(r._p) { _reserve(); }
+        object_ptr(object_ptr&& r) : _p(r._p) { r._p = nullptr; }
+        object_ptr(const object_ptr&&) = delete;
+        ~object_ptr() { _release(); }
+    };
 }
--- slow/include/slow/Type.hpp
+++ slow/include/slow/Type.hpp
@@ -1,98 +1,74 @@
 #pragma once
-#include <cstdint>
 
 // basic type primitives
-namespace slow
-{
-	using u8 = std::uint8_t;
-	using u16 = std::uint16_t;
-	using u32 = std::uint32_t;
-	using u64 = std::uint64_t;
-	using i8 = std::int8_t;
-	using i16 = std::int16_t;
-	using i32 = std::int32_t;
-	using i64 = std::int64_t;
-	using f32 = float;
-	using f64 = double;
-	
-	static_assert(sizeof(u8) == 1);
-	static_assert(sizeof(u16) == 2);
-	static_assert(sizeof(u32) == 4);
-	static_assert(sizeof(u64) == 8);
-	static_assert(sizeof(i8) == 1);
-	static_assert(sizeof(i16) == 2);
-	static_assert(sizeof(i32) == 4);
-	static_assert(sizeof(i64) == 8);
-	static_assert(sizeof(f32) == 4);
-	static_assert(sizeof(f64) == 8);
-}
+#include "slow/NativeType.hpp"
 
 // basic types
 namespace slow
 {
-	struct f32x2
-	{
-		union
-		{
-			struct
-			{
-				f32 x;
-				f32 y;
-			};
-			f32 data[2];
-		};
-		f32x2() : x(0.0f), y(0.0f) {}
-		f32x2(f32 x_, f32 y_) : x(x_), y(y_) {}
-	};
-	struct f32x3
-	{
-		union
-		{
-			struct
-			{
-				f32 x;
-				f32 y;
-				f32 z;
-			};
-			struct
-			{
-				f32 r;
-				f32 g;
-				f32 b;
-			};
-			f32 data[3];
-		};
-		f32x3() : x(0.0f), y(0.0f), z(0.0f) {}
-		f32x3(f32 x_, f32 y_, f32 z_) : x(x_), y(y_), z(z_) {}
-	};
-	struct f32x4
-	{
-		union
-		{
-			struct
-			{
-				f32 x;
-				f32 y;
-				f32 z;
-				f32 w;
-			};
-			struct
-			{
-				f32 r;
-				f32 g;
-				f32 b;
-				f32 a;
-			};
-			struct
-			{
-				f32 left;
-				f32 top;
-				f32 right;
-				f32 bottom;
-			};
-			f32 data[4];
-		};
-		f32x4() : x(0.0f), y(0.0f), z(0.0f), w(0.0f) {}
-		f32x4(f32 x_, f32 y_, f32 z_, f32 w_) : x(x_), y(y_), z(z_), w(w_) {}
-	};
+    struct f32x2
+    {
+        union
+        {
+            struct
+            {
+                f32 x;
+                f32 y;
+            };
+            f32 data[2];
+        };
+        f32x2() : x(0.0f), y(0.0f) {}
+        f32x2(f32 x_, f32 y_) : x(x_), y(y_) {}
+    };
+    struct f32x3
+    {
+        union
+        {
+            struct
+            {
+                f32 x;
+                f32 y;
+                f32 z;
+            };
+            struct
+            {
+                f32 r;
+                f32 g;
+                f32 b;
+            };
+            f32 data[3];
+        };
+        f32x3() : x(0.0f), y(0.0f), z(0.0f) {}
+        f32x3(f32 x_, f32 y_, f32 z_) : x(x_), y(y_), z(z_) {}
+    };
+    struct f32x4
+    {
+        union
+        {
+            struct
+            {
+                f32 x;
+                f32 y;
+                f32 z;
+                f32 w;
+            };
+            struct
+            {
+                f32 r;
+                f32 g;
+                f32 b;
+                f32 a;
+            };
+            struct
+            {
+                f32 left;
+                f32 top;
+                f32 right;
+                f32 bottom;
+            };
+            f32 data[4];
+        };
+        f32x4() : x(0.0f), y(0.0f), z(0.0f), w(0.0f) {}
+        f32x4(f32 x_, f32 y_, f32 z_, f32 w_) : x(x_), y(y_), z(z_), w(w_) {}
+    };
 }
