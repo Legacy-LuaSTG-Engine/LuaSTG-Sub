65042e5d4fae3a3ef0638a9aab640e0708002319
Kuanlan <Kuanlan@outlook.com>
2021-11-29 15:57:51 +0800

    xtype

--- slow/include/slow/Type.hpp
+++ slow/include/slow/Type.hpp
@@ -1,7 +1,89 @@
 #pragma once
 
 // basic type primitives
-#include "slow/NativeType.hpp"
+#ifdef _WIN32
+namespace slow
+{
+    // Win32/Win64 model: ILP32/LLP64
+    // int, long: 32bits
+    // long long: 64bits
+    // pointer: 32/64bits
+    
+    using u8 = unsigned char;
+    using u16 = unsigned short int;
+    using u32 = unsigned int;
+    using u64 = unsigned long long int;
+    
+    static_assert(sizeof(u8) == 1, "invalid u8 type size");
+    static_assert(sizeof(u16) == 2, "invalid u16 type size");
+    static_assert(sizeof(u32) == 4, "invalid u32 type size");
+    static_assert(sizeof(u64) == 8, "invalid u64 type size");
+    
+    using i8 = signed char;
+    using i16 = signed short int;
+    using i32 = signed int;
+    using i64 = signed long long int;
+    
+    static_assert(sizeof(i8) == 1, "invalid i8 type size");
+    static_assert(sizeof(i16) == 2, "invalid i16 type size");
+    static_assert(sizeof(i32) == 4, "invalid i32 type size");
+    static_assert(sizeof(i64) == 8, "invalid i64 type size");
+    
+    using f32 = float;
+    using f64 = double;
+    
+    static_assert(sizeof(f32) == 4, "invalid f32 type size");
+    static_assert(sizeof(f64) == 8, "invalid f64 type size");
+    
+    using c8 = char;
+    using c16 = u16;
+    using c32 = u32;
+    
+    static_assert(sizeof(c8) == 1, "invalid c8 type size");
+    static_assert(sizeof(c16) == 2, "invalid c16 type size");
+    static_assert(sizeof(c32) == 4, "invalid c32 type size");
+    
+    #ifdef _WIN64
+        using usize = u64;
+        using isize = i64;
+    #else
+        using usize = u32;
+        using isize = i32;
+    #endif
+    
+    static_assert(sizeof(usize) == sizeof(void*), "invalid usize type size");
+    static_assert(sizeof(isize) == sizeof(void*), "invalid isize type size");
+}
+#else
+#include <cstdint>
+namespace slow
+{
+    using u8 = uint8_t;
+    using u16 = uint16_t;
+    using u32 = uint32_t;
+    using u64 = uint64_t;
+    using i8 = int8_t;
+    using i16 = int16_t;
+    using i32 = int32_t;
+    using i64 = int64_t;
+    using f32 = float;
+    using f64 = double;
+    #if (__cplusplus >= 202002L)
+        using c8 = char8_t;
+    #else
+        using c8 = char;
+    #endif
+    #if (__cplusplus >= 201103L)
+        using c16 = char16_t;
+        using c32 = char32_t;
+    #else
+        using c16 = u16;
+        using c32 = u32;
+    #endif
+    using usize = size_t;
+    using isize = ptrdiff_t;
+}
+#endif
 
 // basic types
 namespace slow
--- slow/src/make_type.cpp
+++ slow/src/make_type.cpp
@@ -1,9 +1,7 @@
 #include <string>
-#include <string_view>
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <filesystem>
 #include <stdexcept>
 
 const char* match_integer_size(size_t size)
@@ -29,7 +27,7 @@ int main(int argc, char** argv)
     std::string outpath = "";
     for (int c = 0; c < argc; c += 1)
     {
-        if (std::string_view(argv[c]) == "-o")
+        if (std::string(argv[c]) == "-o")
         {
             if ((c + 1) < argc)
             {
@@ -48,16 +46,41 @@ int main(int argc, char** argv)
     header << std::endl;
     header << "namespace slow" << std::endl;
     header << "{" << std::endl;
+    
     header << "    using u8 = unsigned " << match_integer_size(1) << ";" << std::endl;
     header << "    using u16 = unsigned " << match_integer_size(2) << ";" << std::endl;
     header << "    using u32 = unsigned " << match_integer_size(4) << ";" << std::endl;
     header << "    using u64 = unsigned " << match_integer_size(8) << ";" << std::endl;
+    
     header << "    using i8 = signed " << match_integer_size(1) << ";" << std::endl;
     header << "    using i16 = signed " << match_integer_size(2) << ";" << std::endl;
     header << "    using i32 = signed " << match_integer_size(4) << ";" << std::endl;
     header << "    using i64 = signed " << match_integer_size(8) << ";" << std::endl;
+    
     header << "    using f32 = " << match_float_size(4) << ";" << std::endl;
     header << "    using f64 = " << match_float_size(8) << ";" << std::endl;
+    
+    #if (_MSVC_LANG >= 202002L)
+    static_assert(sizeof(char8_t) == 1, "unknown type");
+    constexpr char8_t _test_char8_t = 0;
+    header << "    using c8 = char8_t;" << std::endl;
+    #else
+    static_assert(sizeof(char) == 1, "unknown type");
+    header << "    using c8 = char;" << std::endl;
+    #endif
+    #if (_MSVC_LANG >= 201103L) // MSVC always support c++14 and this required c++11
+    static_assert(sizeof(char16_t) == 2, "unknown type");
+    static_assert(sizeof(char32_t) == 4, "unknown type");
+    header << "    using c16 = char16_t;" << std::endl;
+    header << "    using c32 = char32_t;" << std::endl;
+    #else
+    header << "    using c16 = unsigned " << match_integer_size(2) << ";" << std::endl;
+    header << "    using c32 = unsigned " << match_integer_size(4) << ";" << std::endl;
+    #endif
+    
+    header << "    using usize = unsigned " << match_integer_size(sizeof(void*)) << ";" << std::endl;
+    header << "    using isize = signed " << match_integer_size(sizeof(void*)) << ";" << std::endl;
+    
     header << "}" << std::endl;
     
     std::ofstream header_file(outpath, std::ofstream::out | std::ofstream::trunc | std::ofstream::binary);
