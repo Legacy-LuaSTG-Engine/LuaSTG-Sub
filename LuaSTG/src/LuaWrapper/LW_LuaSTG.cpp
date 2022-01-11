904d5beffc114ec104315c8e10fb5be520280c73
Kuanlan <Kuanlan@outlook.com>
2022-01-11 16:32:28 +0800

    开洞后续

--- LuaSTG/src/LuaWrapper/LW_LuaSTG.cpp
+++ LuaSTG/src/LuaWrapper/LW_LuaSTG.cpp
@@ -1044,8 +1044,16 @@ void BuiltInFunctionWrapper::Register(lua_State* L)LNOEXCEPT
 			if (!pActivedPool)
 				return luaL_error(L, "can't load resource at this time.");
 
-			if (!pActivedPool->LoadFX(name, path))
-				return luaL_error(L, "load fx failed (name=%s, path=%s)", name, path);
+			if (lua_gettop(L) < 3)
+			{
+				if (!pActivedPool->LoadFX(name, path))
+					return luaL_error(L, "load fx failed (name=%s, path=%s)", name, path);
+			}
+			else
+			{
+				if (!pActivedPool->LoadFX(name, path, lua_toboolean(L, 3)))
+					return luaL_error(L, "load fx failed (name=%s, path=%s)", name, path);
+			}
 			return 0;
 		}
 		static int CreateRenderTarget(lua_State* L)LNOEXCEPT
