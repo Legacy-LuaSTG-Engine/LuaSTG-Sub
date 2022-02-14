bc2e671b3e372b5463aa8558f42bf91d553aad19
Kuanlan <Kuanlan@outlook.com>
2022-02-14 11:54:25 +0800

    移除应该不再使用的文件

--- Common/EntryPoint.hpp
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-#include <Windows.h>
-
-#define CMain int main(int argc, char* argv[])
-#define WindowsMain int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
-#define WindowsMainW int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
