6a88f5591a004676a6dfe492d8ed89e6b7c61c5b
璀境石 <Kuanlan@outlook.com>
2022-03-03 03:45:16 +0800

    外部设置工具

---
+++ LuaSTGConfig/pch.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifdef _WIN32_WINNT
+#undef _WIN32_WINNT
+#endif
+#ifdef NTDDI_VERSION
+#undef NTDDI_VERSION
+#endif
+#include <sdkddkver.h>
+
+#include <vector>
+#include <string>
+#include <string_view>
+#include <stdexcept>
+#include <unordered_map>
+#include <tuple>
+#include <fstream>
+#include <filesystem>
+
+#include "utility/encoding.hpp"
+#include "spdlog/spdlog.h"
+#include "nlohmann/json.hpp"
+
+#define NOMINMAX
+#include <Windows.h>
+#include <wrl/client.h>
+#include <wrl/wrappers/corewrappers.h>
+#include <dxgi1_6.h>
+#include <d3d11_4.h>
