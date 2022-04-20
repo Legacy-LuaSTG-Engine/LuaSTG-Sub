cc5ee5bde556a38848c8fd43516874d1939cf475
璀境石 <Kuanlan@outlook.com>
2022-04-20 16:52:30 +0800

    test

--- data/test/src/main.lua
+++ data/test/src/main.lua
@@ -1,5 +1,7 @@
 local timer = 0
 local window = { width = 1920, height = 1080, }
+local Key = require("keycode").Keyboard
+local MKey = require("keycode").Mouse
 local imgui = require("imgui")
 local function set_camera()
     lstg.SetViewport(0, window.width, 0, window.height)
@@ -8,8 +10,91 @@ local function set_camera()
     lstg.SetImageScale(1)
     lstg.SetFog()
 end
+local function Camera3D()
+    ---@class kuanlan.Camera3D
+    local M = {
+        dir = 90,
+        upd = 0,
+        x = 0,
+        y = 0,
+        z = -10,
+        speed = 0.05,
+
+        rbtn = false,
+        mx = 0,
+        my = 0,
+        last_dir = 90,
+        last_upd = 0,
+        aspeed = 0.1,
+    }
+    function M:update()
+        local speed = self.speed
+        if lstg.GetKeyState(Key.CTRL) then
+            speed = speed * 10.0
+        end
+        if lstg.GetKeyState(Key.W) then
+            self.x = self.x + speed * math.cos(math.rad(self.dir))
+            self.z = self.z + speed * math.sin(math.rad(self.dir))
+        elseif lstg.GetKeyState(Key.S) then
+            self.x = self.x - speed * math.cos(math.rad(self.dir))
+            self.z = self.z - speed * math.sin(math.rad(self.dir))
+        end
+        if lstg.GetKeyState(Key.D) then
+            self.x = self.x + speed * math.cos(math.rad(self.dir - 90))
+            self.z = self.z + speed * math.sin(math.rad(self.dir - 90))
+        elseif lstg.GetKeyState(Key.A) then
+            self.x = self.x + speed * math.cos(math.rad(self.dir + 90))
+            self.z = self.z + speed * math.sin(math.rad(self.dir + 90))
+        end
+        if lstg.GetKeyState(Key.SPACE) then
+            self.y = self.y + speed
+        elseif lstg.GetKeyState(Key.SHIFT) then
+            self.y = self.y - speed
+        end
+        if not self.rbtn and lstg.GetMouseState(MKey.RBUTTON) then
+            self.rbtn = true
+            self.mx, self.my = lstg.GetMousePosition()
+            self.last_dir = self.dir
+            self.last_upd = self.upd
+        elseif self.rbtn and not lstg.GetMouseState(MKey.RBUTTON) then
+            self.rbtn = false
+        end
+        if self.rbtn then
+            local mx, my = lstg.GetMousePosition()
+            local dx, dy = mx - self.mx, my - self.my
+            self.dir = self.last_dir - self.aspeed * dx
+            self.upd = self.last_upd + self.aspeed * dy
+            self.upd = math.max(-89.0, math.min(self.upd, 89.0))
+        end
+    end
+    function M:apply()
+        local tx, ty, tz = 1, 0, 0
+        local ux, uy, uz = 0, 1, 0
+        local function vec2_rot(x, y, r_deg)
+            local sin_v = math.sin(math.rad(r_deg))
+            local cos_v = math.cos(math.rad(r_deg))
+            return x * cos_v - y * sin_v, x * sin_v + y * cos_v
+        end
+        tx, ty = vec2_rot(tx, ty, self.upd)
+        ux, uy = vec2_rot(ux, uy, self.upd)
+        tx, tz = vec2_rot(tx, tz, self.dir)
+        ux, uz = vec2_rot(ux, uz, self.dir)
+        lstg.SetViewport(0, window.width, 0, window.height)
+        lstg.SetScissorRect(0, window.width, 0, window.height)
+        lstg.SetPerspective(
+            self.x, self.y, self.z,
+            self.x + tx, self.y + ty, self.z + tz,
+            ux, uy, uz,
+            math.rad(80), window.width / window.height,
+            0.01, 1000.0
+        )
+        lstg.SetImageScale(1)
+        lstg.SetFog(5, 20, lstg.Color(255, 255, 255, 255))
+    end
+    return M
+end
 function GameInit()
-    --lstg.ChangeVideoMode(window.width, window.height, true, true)
+    lstg.ChangeVideoMode(window.width, window.height, true, true)
     set_camera()
     lstg.LoadTTF("Sans", "C:/Windows/Fonts/msyh.ttc", 16, 16)
 end
@@ -23,18 +108,20 @@ function FrameFunc()
     imgui.backend.ShowMemoryUsageWindow()
     imgui.backend.ShowFrameStatistics()
     imgui.ImGui.EndFrame()
-	return false
+    return false
 end
 function RenderFunc()
     lstg.BeginScene()
     lstg.RenderClear(lstg.Color(255, 255, 255, 255))
+    lstg.ClearZBuffer(1.0)
+
     set_camera()
     lstg.RenderTTF("Sans", "您好，别来无恙啊！", 0, 0, 720, 720, 0 + 0, lstg.Color(255, 0, 0, 0), 2)
 
     RenderTTF3(
         "Sans", "旋转测试 ++++ 测试文本，。……",
         window.width / 2, window.height / 2,
-        timer / 4,
+        timer,
         4, 4,
         "", lstg.Color(255, 0, 0, 0),
         "center", "vcenter")
