a84621136756921f51ef105881ccf3a91ee13e7b
璀境石 <Kuanlan@outlook.com>
2022-04-21 20:11:11 +0800

    减少 Windows 头文件解析用时

--- utility/utility/encoding.cpp
+++ utility/utility/encoding.cpp
@@ -1,15 +1,17 @@
 #include "utility/encoding.hpp"
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
 namespace utility::encoding
 {
     std::string to_utf8(std::wstring_view wide_string)
     {
-        int const size = ::WideCharToMultiByte(CP_UTF8, 0, wide_string.data(), (int)wide_string.length(), NULL, 0, NULL, NULL);
+        int const size = WideCharToMultiByte(CP_UTF8, 0, wide_string.data(), (int)wide_string.length(), NULL, 0, NULL, NULL);
         if (size > 0)
         {
             std::string buf(size, '\0');
-            int const result = ::WideCharToMultiByte(CP_UTF8, 0, wide_string.data(), (int)wide_string.length(), buf.data(), size, NULL, NULL);
+            int const result = WideCharToMultiByte(CP_UTF8, 0, wide_string.data(), (int)wide_string.length(), buf.data(), size, NULL, NULL);
             if (result == size)
             {
                 return std::move(buf);
@@ -19,11 +21,11 @@ namespace utility::encoding
     }
     std::wstring to_wide(std::string_view utf8_string)
     {
-        int const size = ::MultiByteToWideChar(CP_UTF8, 0, utf8_string.data(), (int)utf8_string.length(), NULL, 0);
+        int const size = MultiByteToWideChar(CP_UTF8, 0, utf8_string.data(), (int)utf8_string.length(), NULL, 0);
         if (size > 0)
         {
             std::wstring buf(size, L'\0');
-            int const result = ::MultiByteToWideChar(CP_UTF8, 0, utf8_string.data(), (int)utf8_string.length(), buf.data(), size);
+            int const result = MultiByteToWideChar(CP_UTF8, 0, utf8_string.data(), (int)utf8_string.length(), buf.data(), size);
             if (result == size)
             {
                 return std::move(buf);
@@ -37,11 +39,11 @@ namespace utility::encoding
     }
     std::string to_ansi(std::wstring_view wide_string)
     {
-        int const size = ::WideCharToMultiByte(CP_ACP, 0, wide_string.data(), (int)wide_string.length(), NULL, 0, NULL, NULL);
+        int const size = WideCharToMultiByte(CP_ACP, 0, wide_string.data(), (int)wide_string.length(), NULL, 0, NULL, NULL);
         if (size > 0)
         {
             std::string buf(size, '\0');
-            int const result = ::WideCharToMultiByte(CP_ACP, 0, wide_string.data(), (int)wide_string.length(), buf.data(), size, NULL, NULL);
+            int const result = WideCharToMultiByte(CP_ACP, 0, wide_string.data(), (int)wide_string.length(), buf.data(), size, NULL, NULL);
             if (result == size)
             {
                 return std::move(buf);
