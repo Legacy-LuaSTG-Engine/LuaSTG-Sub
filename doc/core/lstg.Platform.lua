75e378069ce09326c7f89176c4bd3805e91cbcf3
璀境石 <Kuanlan@outlook.com>
2022-05-08 06:45:41 +0800

    更新文档

--- doc/core/lstg.Platform.lua
+++ doc/core/lstg.Platform.lua
@@ -1,12 +1,12 @@
 --------------------------------------------------------------------------------
---- LuaSTG Sub 杂项功能
+--- LuaSTG Sub 平台拓展
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
---- 平台调用
+--- Win32 API
 
 --- 警告：该功能慎用，可能会被杀毒软件误杀
---- 注意：该 API 未来可能被移除，并作为平台拓展提供
+--- 注意：该 API 未来可能会被移除，并作为 Windows 平台拓展提供
 --- wait 和 show 如果不填写，默认为 true
 ---@param path string
 ---@param arg string
@@ -19,4 +19,12 @@
 ---@overload fun(path:string, arg :string, workingdir:string):boolean
 ---@overload fun(path:string, arg :string, workingdir:string, wait:boolean):boolean
 function lstg.Execute(path, arg, workingdir, wait, show)
+end
+
+--- 注意：该 API 未来可能会被移除，并作为 Windows 平台拓展提供
+---@param title string
+---@param text string
+---@param flags number
+---@return number
+function lstg.MessageBox(title, text, flags)
 end
