5f7e9c0703a26ddf928f252f8337f2583684eb1d
璀境石 <Kuanlan@outlook.com>
2022-05-26 21:01:47 +0800

    事件族

--- LuaSTG/Core/Graphics/Device.hpp
+++ LuaSTG/Core/Graphics/Device.hpp
@@ -3,9 +3,17 @@
 
 namespace LuaSTG::Core::Graphics
 {
+	struct IDeviceEventListener
+	{
+		virtual void onDeviceCreate() = 0;
+		virtual void onDeviceDestroy() = 0;
+	};
+
 	struct IDevice : public IObject
 	{
-	public:
+		virtual void addEventListener(IDeviceEventListener* e) = 0;
+		virtual void removeEventListener(IDeviceEventListener* e) = 0;
+
 		static bool create(StringView prefered_gpu, IDevice** p_device);
 	};
 }
