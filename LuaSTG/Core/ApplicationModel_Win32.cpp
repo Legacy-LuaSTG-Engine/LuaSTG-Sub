a3a966822d9d968022c3a6da3e194096314a3a12
璀境石 <Kuanlan@outlook.com>
2022-05-29 18:54:39 +0800

    奇怪的bug x2

--- LuaSTG/Core/ApplicationModel_Win32.cpp
+++ LuaSTG/Core/ApplicationModel_Win32.cpp
@@ -312,7 +312,7 @@ namespace LuaSTG::Core
 			if (win32_events[0])
 			{
 				// 监控退出事件和消息队列
-				switch (MsgWaitForMultipleObjectsEx(1, win32_events, INFINITE, QS_ALLINPUT, 0))
+				switch (MsgWaitForMultipleObjectsEx(1, win32_events, 1000, QS_ALLINPUT, 0))
 				{
 				case (WAIT_OBJECT_0):
 					// 退出信号不是由窗口触发的，而是由工作线程触发的
