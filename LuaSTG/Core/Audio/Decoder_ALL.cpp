54f9e046a9eb4e8da5b4933fb4c5c3622d7b10c5
璀境石 <Kuanlan@outlook.com>
2022-06-10 13:51:50 +0800

    Core 解码器

---
+++ LuaSTG/Core/Audio/Decoder_ALL.cpp
@@ -0,0 +1,30 @@
+﻿#include "Core/Audio/Decoder.hpp"
+#include "Core/Audio/Decoder_WAV.hpp"
+#include "Core/Audio/Decoder_VorbisOGG.hpp"
+
+namespace LuaSTG::Core::Audio
+{
+	bool IDecoder::create(StringView path, IDecoder** pp_decoder)
+	{
+		try
+		{
+			*pp_decoder = new Decoder_WAV(path);
+			return true;
+		}
+		catch (std::exception const& e)
+		{
+			spdlog::error("[core] {}", e.what());
+		}
+		try
+		{
+			*pp_decoder = new Decoder_VorbisOGG(path);
+			return true;
+		}
+		catch (std::exception const& e)
+		{
+			spdlog::error("[core] {}", e.what());
+		}
+		*pp_decoder = nullptr;
+		return false;
+	}
+}
---
+++ LuaSTG/Core/Audio/Decoder_WAV.cpp
@@ -0,0 +1,90 @@
+﻿#include "Core/Audio/Decoder_WAV.hpp"
+#include "Core/FileManager.hpp"
+#include "utility/encoding.hpp"
+
+namespace LuaSTG::Core::Audio
+{
+	uint32_t Decoder_WAV::getFrameCount()
+	{
+		drwav_uint64 dr_frame_count = 0;
+		drwav_result const result = drwav_get_length_in_pcm_frames(&m_wav, &dr_frame_count);
+		assert(DRWAV_SUCCESS == result);
+		return (uint32_t)dr_frame_count;
+	}
+
+	bool Decoder_WAV::seek(uint32_t pcm_frame)
+	{
+		return DRWAV_TRUE == drwav_seek_to_pcm_frame(&m_wav, pcm_frame);
+	}
+	bool Decoder_WAV::seekByTime(double sec)
+	{
+		drwav_uint64 const pcm_frame = drwav_uint64(sec * (double)m_wav.sampleRate);
+		return DRWAV_TRUE == drwav_seek_to_pcm_frame(&m_wav, pcm_frame);
+	}
+	bool Decoder_WAV::tell(uint32_t* pcm_frame)
+	{
+		drwav_uint64 dr_frame_index = 0;
+		drwav_result const result = drwav_get_cursor_in_pcm_frames(&m_wav, &dr_frame_index);
+		*pcm_frame = (uint32_t)dr_frame_index;
+		return DRWAV_SUCCESS == result;
+	}
+	bool Decoder_WAV::tellAsTime(double* sec)
+	{
+		drwav_uint64 dr_frame_index = 0;
+		drwav_result const result = drwav_get_cursor_in_pcm_frames(&m_wav, &dr_frame_index);
+		*sec = (double)dr_frame_index / (double)m_wav.sampleRate;
+		return DRWAV_SUCCESS == result;
+	}
+	bool Decoder_WAV::read(uint32_t pcm_frame, void* buffer, uint32_t* read_pcm_frame)
+	{
+		drwav_uint64 const dr_frame_count = drwav_read_pcm_frames_s16(&m_wav, pcm_frame, (drwav_int16*)buffer);
+		if (read_pcm_frame)
+		{
+			*read_pcm_frame = (uint32_t)dr_frame_count;
+			return pcm_frame == 0 || dr_frame_count > 0; // 似乎永远不会失败
+		}
+		else
+		{
+			assert(pcm_frame == dr_frame_count);
+			return pcm_frame == dr_frame_count;
+		}
+	}
+
+	Decoder_WAV::Decoder_WAV(StringView path)
+		: m_wav({})
+		, m_init(false)
+	{
+		if (GFileManager().contain(path))
+		{
+			// 存在于文件系统，直接以文件的形式打开，一般 wav 都贼 TM 大
+			drwav_bool32 const result = drwav_init_file_w(&m_wav, utility::encoding::to_wide(path).c_str(), NULL);
+			if (DRWAV_TRUE != result)
+				throw std::runtime_error("Decoder_WAV::Decoder_WAV (1)");
+		}
+		else if (GFileManager().containEx(path))
+		{
+			// 在压缩包里的文件，只能读取到内存了
+			if (!GFileManager().loadEx(path, m_data))
+				throw std::runtime_error("Decoder_WAV::Decoder_WAV (2)");
+			drwav_bool32 const result = drwav_init_memory(&m_wav, m_data.data(), m_data.size(), NULL);
+			if (DRWAV_TRUE != result)
+				throw std::runtime_error("Decoder_WAV::Decoder_WAV (3)");
+		}
+		else
+		{
+			throw std::runtime_error("Decoder_WAV::Decoder_WAV (4)");
+		}
+		m_init = true;
+		// 先逝一逝
+		if (!seek(0))
+			throw std::runtime_error("Decoder_WAV::Decoder_WAV (5)");
+	}
+	Decoder_WAV::~Decoder_WAV()
+	{
+		if (m_init)
+		{
+			m_init = false;
+			drwav_uninit(&m_wav);
+		}
+	}
+}
