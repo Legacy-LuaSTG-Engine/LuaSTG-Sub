828abfea35eb355115be8362391d79fee432498f
璀境石 <Kuanlan@outlook.com>
2022-06-17 15:55:40 +0800

    改进的环形队列

--- LuaSTG/cpp/CircularQueue.hpp
+++ LuaSTG/cpp/CircularQueue.hpp
@@ -9,9 +9,9 @@ namespace LuaSTGPlus
 	{
 	private:
 		std::array<T, MaxSize> m_Data;
-		size_t m_Front = 0;//头部索引
-		size_t m_Rear = 0; //下一个可置入的对象的索引
-		size_t m_Count = 0;//已用空间
+		size_t m_Front = 0; // 头部索引
+		size_t m_Rear  = 0; // 下一个可置入的对象的索引
+		size_t m_Count = 0; // 已用空间
 	public:
 		T& operator[](size_t idx)
 		{
@@ -19,14 +19,75 @@ namespace LuaSTGPlus
 			return m_Data[(idx + m_Front) % MaxSize];
 		}
 	public:
-		//队列是否为空
-		bool IsEmpty() { return m_Count == 0; }
-		//队列是否已满
-		bool IsFull() { return m_Count >= MaxSize; }
-		//返回已经使用的空间
-		size_t Size() { return m_Count; }
-		//返回最大索引
-		size_t Max() { return MaxSize - 1; }
+		// 队列是否为空
+		bool IsEmpty() const noexcept { return m_Count == 0; }
+		// 队列是否已满
+		bool IsFull() const noexcept { return m_Count >= MaxSize; }
+		// 返回已经使用的空间
+		size_t Size() const noexcept { return m_Count; }
+		// 返回最大容量
+		constexpr size_t Capacity() const noexcept { return MaxSize; }
+		// 重置
+		void Clear() noexcept { m_Front = 0; m_Rear = 0; m_Count = 0; }
+		// 预分配空间
+		void PlacementResize(size_t size) { assert(size <= MaxSize); m_Rear = size; m_Count = size; }
+		// 在尾部插入对象，仅分配空间，不产生复制
+		T& PlacementPushTail()
+		{
+			assert(!IsFull());
+			T& Data = m_Data[m_Rear]; // 当前索引的位置
+			m_Rear = (m_Rear + 1) % MaxSize; // 索引后移
+			m_Count += 1;
+			return Data;
+		}
+		// 在头部插入对象，仅分配空间，不产生复制
+		T& PlacementPushHead()
+		{
+			assert(!IsFull());
+			m_Front = (m_Front + MaxSize - 1) % MaxSize; // 头部索引前移
+			T& Data = m_Data[m_Front]; // 头部索引的位置
+			m_Count += 1;
+			return Data;
+		}
+		// 在尾部插入对象
+		void PushTail(T const& value)
+		{
+			assert(!IsFull());
+			m_Data[m_Rear] = value; // 当前索引的位置
+			m_Rear = (m_Rear + 1) % MaxSize; // 索引后移
+			m_Count += 1;
+		}
+		// 在头部插入对象
+		void PushHead(T const& value)
+		{
+			assert(!IsFull());
+			m_Front = (m_Front + MaxSize - 1) % MaxSize; // 头部索引前移
+			m_Data[m_Front] = value; // 头部索引的位置
+			m_Count += 1;
+		}
+		// 从尾部移除一个对象
+		T& PopTail()
+		{
+			assert(!IsEmpty());
+			m_Rear = (m_Rear + MaxSize - 1) % MaxSize; // 当前索引前移
+			T& Data = m_Data[m_Rear]; // 当前索引的位置
+			m_Count -= 1;
+			return Data;
+		}
+		// 从头部移除一个对象
+		T& PopHead()
+		{
+			assert(!IsEmpty());
+			T& Data = m_Data[m_Front]; // 头部索引的位置
+			m_Front = (m_Front + 1) % MaxSize; // 头部索引后移
+			m_Count -= 1;
+			return Data;
+		}
+		// 访问尾部
+		T& Tail() { assert(!IsEmpty()); return m_Data[m_Rear + MaxSize - 1]; }
+		// 访问头部
+		T& Head() { assert(!IsEmpty()); return m_Data[m_Front]; }
+	public:
 		//在尾部置入一个对象，如果循环队列已满则返回false
 		bool Push(T val)
 		{
