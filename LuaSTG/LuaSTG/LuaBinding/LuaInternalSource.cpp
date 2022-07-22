52785d8ce7b2e95b7aae12e74b00c87096b7ecc2
璀境石 <Kuanlan@outlook.com>
2022-07-22 22:11:27 +0800

    SystemLog 和 Print 用 lua 实现；print 用 Print 覆盖

--- LuaSTG/LuaSTG/LuaBinding/LuaInternalSource.cpp
+++ LuaSTG/LuaSTG/LuaBinding/LuaInternalSource.cpp
@@ -62,6 +62,19 @@ end
 function lstg.PostEffectApply()
 end
 
+function lstg.SystemLog(text)
+    lstg.Log(2, text)
+end
+
+function lstg.Print(...)
+    local args = {...}
+    local argc = select('#', ...)
+    for i = 1, argc do args[i] = tostring(args[i]) end
+    lstg.Log(2, table.concat(args, '\t'))
+end
+
+print = lstg.Print
+
 if cjson then
     package.loaded["cjson"] = cjson -- fuck you cjson
 end
