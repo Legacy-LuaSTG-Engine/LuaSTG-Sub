0bef94eab5c73397568e7beee380aa2c2ffd545b
Kuanlan <Kuanlan@outlook.com>
2022-08-22 01:22:04 +0800

    qoi test

--- data/test/src/test_texture.lua
+++ data/test/src/test_texture.lua
@@ -1,7 +1,7 @@
 local test = require("test")
 
 local function load_texture(f)
-    lstg.LoadTexture("tex:" .. f, "res/" .. f .. ".png", false)
+    lstg.LoadTexture("tex:" .. f, "res/" .. f, false)
     local w, h = lstg.GetTextureSize("tex:" .. f)
     lstg.LoadImage("img:" .. f, "tex:" .. f, 0, 0, w, h)
 end
@@ -17,15 +17,19 @@ function M:onCreate()
     local old_pool = lstg.GetResourceStatus()
     lstg.SetResourceStatus("global")
 
-    load_texture("sRGB")
-    load_texture("linear")
+    load_texture("sRGB.png")
+    load_texture("linear.png")
+    load_texture("block.png")
+    load_texture("block.qoi")
 
     lstg.SetResourceStatus(old_pool)
 end
 
 function M:onDestroy()
-    unload_texture("sRGB")
-    unload_texture("linear")
+    unload_texture("sRGB.png")
+    unload_texture("linear.png")
+    unload_texture("block.png")
+    unload_texture("block.qoi")
 end
 
 function M:onUpdate()
@@ -34,8 +38,11 @@ end
 function M:onRender()
     window:applyCameraV()
     local scale = 0.5
-    lstg.Render("img:sRGB", window.width / 4 * 1, window.height / 2, 0, scale)
-    lstg.Render("img:linear", window.width / 4 * 3, window.height / 2, 0, scale)
+    lstg.Render("img:sRGB.png", window.width / 4 * 1, window.height / 2, 0, scale)
+    lstg.Render("img:linear.png", window.width / 4 * 3, window.height / 2, 0, scale)
+    scale = 0.5
+    lstg.Render("img:block.png", window.width / 4 * 2, window.height / 4 * 1, 0, scale)
+    lstg.Render("img:block.qoi", window.width / 4 * 2, window.height / 4 * 3, 0, scale)
 end
 
 test.registerTest("test.Module.Texture", M)
