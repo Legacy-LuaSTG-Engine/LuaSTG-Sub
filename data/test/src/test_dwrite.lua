94148e3b4f26fc32c1acb5f5e77161d218b769b7
Kuanlan <Kuanlan@outlook.com>
2022-09-01 18:14:26 +0800

    当绘制带描边的字体布局时，不应该修改字体布局（缩窄）后再渲染

--- data/test/src/test_dwrite.lua
+++ data/test/src/test_dwrite.lua
@@ -3,7 +3,9 @@ local test = require("test")
 ---@class test.Module.DirectWrite : test.Base
 local M = {}
 
-local text = [[People communicate with text all the time in their daily lives. It is the primary way for people to consume an increasing volume of information. In the past, it used to be through printed content, primarily documents, newspapers, books, and so on. Increasingly, it is online content on their Windows PC. A typical Windows user spends a lot of time reading from their computer screen. They might be surfing the Web, scanning e-mail, composing a report, filling in a spreadsheet, or writing software, but what they're really doing is reading. Even though text and fonts permeate nearly every part of the user experience in Windows, for most users, reading on the screen is not as enjoyable as reading printed output.]]
+local text = [[People communicate with text all the time in their daily lives. It is the primary way for people to consume an increasing volume of information. In the past, it used to be through printed content, primarily documents, newspapers, books, and so on. Increasingly, it is online content on their Windows PC. A typical Windows user spends a lot of time reading from their computer screen. They might be surfing the Web, scanning e-mail, composing a report, filling in a spreadsheet, or writing software, but what they're really doing is reading. Even though text and fonts permeate nearly every part of the user experience in Windows, for most users, reading on the screen is not as enjoyable as reading printed output.
+
+统一的 App Store 让你的应用在 Windows 设备（如电脑、平板电脑、Xbox、HoloLens、Surface Hub 和物联网 (IoT) 设备）上可用。 可以向 Microsoft Store 提交应用，并使其对所有类型的设备或仅对所选设备类型可用。 你将在一个位置上提交和管理适用于 Windows 设备的所有应用。 想要使用 UWP 功能实现 C++ 桌面应用的现代化并将其在 Microsoft store 内出售？ 这同样可以实现。]]
 
 function M:onCreate()
     local DirectWrite = require("DirectWrite")
@@ -31,13 +33,13 @@ function M:onCreate()
         640 -- height
     )
     text_layout:SetFontFamilyName("思源宋体 CN", 0, 5)
-    text_layout:SetStrikethrough(true, 0, 5)
-    text_layout:SetUnderline(true, 0, 5)
+    --text_layout:SetStrikethrough(true, 0, 5)
+    --text_layout:SetUnderline(true, 0, 5)
     text_layout:SetFontSize(48, 0, 5)
     text_layout:SetFontStyle(DirectWrite.FontStyle.Oblique, 30, 4)
     text_layout:SetFontFamilyName("思源宋体 CN", 57, 5)
     text_layout:SetFontFamilyName("思源宋体 CN", 63, 5)
-    DirectWrite.CreateTextureFromTextLayout(text_layout, "global", "text-texture")
+    DirectWrite.CreateTextureFromTextLayout(text_layout, "global", "text-texture", 4)
 
     local old_pool = lstg.GetResourceStatus()
     lstg.SetResourceStatus("global")
