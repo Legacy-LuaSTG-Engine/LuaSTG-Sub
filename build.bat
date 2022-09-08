8e356287925ecd3db87b79912bab0a7460bdd92d
璀境石 <Kuanlan@outlook.com>
2022-09-08 23:52:56 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -15,7 +15,7 @@
     :: x86
     @echo ============================ build Win32 ===========================
     cmake -S %SOURCE% -B %BUILD32% -G "Visual Studio 17 2022" -T v143 -A Win32 -DCMAKE_SYSTEM_VERSION="10.0.22000.0"
-    cmake --build %BUILD32% --target LuaTG --config Release --clean-first
+    cmake --build %BUILD32% --target LuaSTG --config Release --clean-first
     ::cmake --build %BUILD32% --target ALL_BUILD --config Release        --clean-first
     ::cmake --build %BUILD32% --target ALL_BUILD --config Debug          --clean-first
     ::cmake --build %BUILD32% --target ALL_BUILD --config RelWithDebInfo --clean-first
@@ -24,7 +24,7 @@
     :: amd64
     @echo ============================= build x64 ============================
     cmake -S %SOURCE% -B %BUILD64% -G "Visual Studio 17 2022" -T v143 -A x64 -DCMAKE_SYSTEM_VERSION="10.0.22000.0"
-    cmake --build %BUILD64% --target LuaTG --config Release --clean-first
+    cmake --build %BUILD64% --target LuaSTG --config Release --clean-first
     ::cmake --build %BUILD64% --target ALL_BUILD --config Release        --clean-first
     ::cmake --build %BUILD64% --target ALL_BUILD --config Debug          --clean-first
     ::cmake --build %BUILD64% --target ALL_BUILD --config RelWithDebInfo --clean-first
