5b68a7189c1df020dffa5e0871747f406c4f4bdb
Kuanlan <Kuanlan@outlook.com>
2022-09-25 13:52:29 +0800

    更多提示和引导

--- LuaSTG/LuaSTG/Main.cpp
+++ LuaSTG/LuaSTG/Main.cpp
@@ -6,6 +6,7 @@
 #include "AppFrame.h"
 
 void openWebsiteWindows7EndOfSupport();
+void openWebsiteDownloadWindows7SP1();
 void openWebsiteDownloadPlatformUpdateForWindows7();
 
 int main()
@@ -19,20 +20,53 @@ int main()
 	{
 		if (!platform::WindowsVersion::Is7SP1())
 		{
-			platform::MessageBox::Error(LUASTG_INFO,
-				"Minimum system requirements are not met.\n"
-				"Satisfied:\n"
-				"    N/A\n"
-				"Not Satisfied:\n"
-				"    Windows 7 SP1\n"
-				"    Platform Update for Windows 7 (KB2670838)\n"
-				"\n"
-				"未达到最低系统要求。\n"
-				"已满足的条件：\n"
-				"    无\n"
-				"未满足的条件：\n"
-				"    Windows 7 SP1\n"
-				"    Platform Update for Windows 7 (KB2670838)");
+			if (!platform::WindowsVersion::Is7())
+			{
+				// 连 Windows 7 都没达到
+				platform::MessageBox::Error(LUASTG_INFO,
+					"Minimum system requirements are not met.\n"
+					"Satisfied:\n"
+					"    N/A\n"
+					"Not Satisfied:\n"
+					"    Windows 7\n"
+					"    Windows 7 Service Pack 1\n"
+					"    Platform Update for Windows 7 (KB2670838)\n"
+					"\n"
+					"未达到最低系统要求。\n"
+					"已满足的条件：\n"
+					"    N/A\n"
+					"未满足的条件：\n"
+					"    Windows 7\n"
+					"    Windows 7 Service Pack 1\n"
+					"    Platform Update for Windows 7 (KB2670838)");
+			}
+			else
+			{
+				// 没装 Windows 7 SP1
+				bool const ok = platform::MessageBox::Warning(LUASTG_INFO,
+					"Minimum system requirements are not met.\n"
+					"Satisfied:\n"
+					"    Windows 7\n"
+					"Not Satisfied:\n"
+					"    Windows 7 Service Pack 1\n"
+					"    Platform Update for Windows 7 (KB2670838)\n"
+					"Click the \"OK\" button to open Microsoft's official website to download the update.\n"
+					"Click the \"Cancel\" button to exit.\n"
+					"\n"
+					"未达到最低系统要求。\n"
+					"已满足的条件：\n"
+					"    Windows 7\n"
+					"未满足的条件：\n"
+					"    Windows 7 Service Pack 1\n"
+					"    Platform Update for Windows 7 (KB2670838)\n"
+					"点击“确定”打开微软官方网站下载更新。\n"
+					"点击“取消”关闭程序。");
+				if (ok)
+				{
+					openWebsiteDownloadWindows7SP1();
+					openWebsiteDownloadPlatformUpdateForWindows7();
+				}
+			}
 		}
 		else
 		{
@@ -40,7 +74,8 @@ int main()
 			bool const ok = platform::MessageBox::Warning(LUASTG_INFO,
 				"Minimum system requirements are not met.\n"
 				"Satisfied:\n"
-				"    Windows 7 SP1\n"
+				"    Windows 7\n"
+				"    Windows 7 Service Pack 1\n"
 				"Not Satisfied:\n"
 				"    Platform Update for Windows 7 (KB2670838)\n"
 				"Click the \"OK\" button to open Microsoft's official website to download the update.\n"
@@ -48,7 +83,8 @@ int main()
 				"\n"
 				"未达到最低系统要求。\n"
 				"已满足的条件：\n"
-				"    Windows 7 SP1\n"
+				"    Windows 7\n"
+				"    Windows 7 Service Pack 1\n"
 				"未满足的条件：\n"
 				"    Platform Update for Windows 7 (KB2670838)\n"
 				"点击“确定”打开微软官方网站下载更新。\n"
@@ -109,9 +145,13 @@ void openWebsiteWindows7EndOfSupport()
 {
 	ShellExecuteW(NULL, NULL, L"https://www.microsoft.com/en-us/windows/end-of-support", NULL, NULL, SW_SHOWNORMAL);
 }
+void openWebsiteDownloadWindows7SP1()
+{
+	ShellExecuteW(NULL, NULL, L"https://www.catalog.update.microsoft.com/Search.aspx?q=KB976932", NULL, NULL, SW_SHOWNORMAL);
+}
 void openWebsiteDownloadPlatformUpdateForWindows7()
 {
-	ShellExecuteW(NULL, NULL, L"https://www.microsoft.com/en-us/download/details.aspx?id=36805", NULL, NULL, SW_SHOWNORMAL);
+	ShellExecuteW(NULL, NULL, L"https://www.catalog.update.microsoft.com/Search.aspx?q=KB2670838", NULL, NULL, SW_SHOWNORMAL);
 }
 
 _Use_decl_annotations_ int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
