972e82e10839bc51725c633df4670bb71c6bbaa6
璀境石 <Kuanlan@outlook.com>
2022-11-22 15:29:54 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -6,15 +6,21 @@
     @set BUILD=%cd%\build
     @set BUILD32=%BUILD%\x86
     @set BUILD64=%BUILD%\amd64
+    @set INSTALL=%cd%\install
+    @set INSTALL32=%INSTALL%\x86
+    @set INSTALL32=%INSTALL%\amd64
     
     :: build and install directory
     mkdir %BUILD%
     mkdir %BUILD32%
     mkdir %BUILD64%
+    mkdir %INSTALL%
+    mkdir %INSTALL32%
+    mkdir %INSTALL64%
     
     :: x86
     @echo ============================ build Win32 ===========================
-    cmake -S %SOURCE% -B %BUILD32% -G "Visual Studio 17 2022" -T v143 -A Win32 -DCMAKE_SYSTEM_VERSION="10.0.22000.0"
+    cmake -S %SOURCE% -B %BUILD32% -G "Visual Studio 17 2022" -T v143 -A Win32 -DCMAKE_SYSTEM_VERSION="10.0.22621.0" -DCMAKE_PREFIX_PATH=%INSTALL32% -DCMAKE_INSTALL_PREFIX=%INSTALL32%
     cmake --build %BUILD32% --target LuaSTG --config Release --clean-first
     ::cmake --build %BUILD32% --target ALL_BUILD --config Release        --clean-first
     ::cmake --build %BUILD32% --target ALL_BUILD --config Debug          --clean-first
@@ -23,8 +29,8 @@
     
     :: amd64
     @echo ============================= build x64 ============================
-    cmake -S %SOURCE% -B %BUILD64% -G "Visual Studio 17 2022" -T v143 -A x64 -DCMAKE_SYSTEM_VERSION="10.0.22000.0"
-    cmake --build %BUILD64% --target LuaSTG --config Release --clean-first
+    ::cmake -S %SOURCE% -B %BUILD64% -G "Visual Studio 17 2022" -T v143 -A x64 -DCMAKE_SYSTEM_VERSION="10.0.22621.0" -DCMAKE_PREFIX_PATH=%INSTALL32% -DCMAKE_INSTALL_PREFIX=%INSTALL32%
+    ::cmake --build %BUILD64% --target LuaSTG --config Release --clean-first
     ::cmake --build %BUILD64% --target ALL_BUILD --config Release        --clean-first
     ::cmake --build %BUILD64% --target ALL_BUILD --config Debug          --clean-first
     ::cmake --build %BUILD64% --target ALL_BUILD --config RelWithDebInfo --clean-first
