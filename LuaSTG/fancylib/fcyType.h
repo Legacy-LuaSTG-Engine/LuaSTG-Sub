ff6d663372cf6e51a77c9620d6d6d7e4964f4afb
Kuanlan <Kuanlan@outlook.com>
2023-01-18 11:42:05 +0800

    继续清理 fcylib

--- LuaSTG/fancylib/fcyType.h
+++ LuaSTG/fancylib/fcyType.h
@@ -4,21 +4,6 @@
 
 #include <cstdint>
 
-typedef char const*         fcStr;   // C式字符串常量
-typedef uint8_t*            fData;   // 内存数据指针
-typedef uint8_t const*      fcData;  // 常量内存数据指针
-typedef int32_t             fResult; // 预定义返回值
-                                     // fResult 定义如下
-                                     //   符号位  ：
-                                     //     0 - 成功
-                                     //     1 - 失败
-                                     //   30-16 位：
-                                     //     保留，置为0
-                                     //   15-0  位：
-                                     //     描述错误类型
-
 #define FCYSAFEKILL(x)   { if (x) { (x)->Release(); (x) = NULL; } }
-
-#include "fcyResult.h"
 
 #endif //__FTYPE__H__
