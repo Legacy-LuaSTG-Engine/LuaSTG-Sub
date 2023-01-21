3bc965c0e50b170817f1b29e89f6730b3a779dd8
璀境石 <Kuanlan@outlook.com>
2023-01-21 18:40:02 +0800

    一次性推送3个仓库

---
+++ cmake/push.ps1
@@ -0,0 +1,26 @@
+# 这个脚本用于一次性推送给 3 个远程仓库
+# 这并非是给所有人用的
+
+# 如果要更新子模块，请使用
+# git submodule update --init --recursive
+
+# 配置工作目录
+
+Set-Location -Path ($PSScriptRoot + "/..")
+$CurrentLocation = Get-Location
+Write-Output ("当前工作目录是：" + $CurrentLocation)
+
+# 推送
+
+[string] $Git = "git"
+
+Write-Output "---------- push ----------"
+
+Write-Output "git push"
+& $Git push github
+Write-Output "git push gitlab"
+& $Git push gitlab
+Write-Output "git push gitee"
+& $Git push gitee
+
+Write-Output "---------- exit ----------"
