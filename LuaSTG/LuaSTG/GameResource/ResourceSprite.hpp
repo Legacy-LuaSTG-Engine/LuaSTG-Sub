2cc17e9b2c115c29a25c03c70066b15b4e1c470f
璀境石 <Kuanlan@outlook.com>
2023-01-22 11:31:57 +0800

    迁移 ResourceSprite

--- LuaSTG/LuaSTG/GameResource/ResourceSprite.hpp
+++ LuaSTG/LuaSTG/GameResource/ResourceSprite.hpp
@@ -4,23 +4,13 @@
 
 namespace LuaSTGPlus
 {
-	class ResSprite : public ResourceBase
+	struct IResourceSprite : public IResourceBase
 	{
-	private:
-		Core::ScopeObject<Core::Graphics::ISprite> m_sprite;
-		BlendMode m_BlendMode = BlendMode::MulAlpha;
-		double m_HalfSizeX = 0.0;
-		double m_HalfSizeY = 0.0;
-		bool m_bRectangle = false;
-	public:
-		Core::Graphics::ISprite* GetSprite() noexcept { return m_sprite.get(); }
-		BlendMode GetBlendMode() const noexcept { return m_BlendMode; }
-		void SetBlendMode(BlendMode m) noexcept { m_BlendMode = m; }
-		double GetHalfSizeX() const noexcept { return m_HalfSizeX; }
-		double GetHalfSizeY() const noexcept { return m_HalfSizeY; }
-		bool IsRectangle() const noexcept { return m_bRectangle; }
-	public:
-		ResSprite(const char* name, Core::Graphics::ISprite* sprite, double hx, double hy, bool rect);
-		~ResSprite();
+		virtual Core::Graphics::ISprite* GetSprite() = 0;
+		virtual BlendMode GetBlendMode() = 0;
+		virtual void SetBlendMode(BlendMode m) = 0;
+		virtual double GetHalfSizeX() = 0;
+		virtual double GetHalfSizeY() = 0;
+		virtual bool IsRectangle() = 0;
 	};
 }
