d0dacb848944f9872a47eda257f6967e5c14bf31
璀境石 <Kuanlan@outlook.com>
2023-01-22 11:32:23 +0800

    迁移 ResourceAnimation

--- LuaSTG/LuaSTG/GameResource/ResourceAnimation.hpp
+++ LuaSTG/LuaSTG/GameResource/ResourceAnimation.hpp
@@ -1,36 +1,20 @@
 ﻿#pragma once
 #include "GameResource/ResourceBase.hpp"
-#include "GameResource/ResourceTexture.hpp"
 #include "Core/Graphics/Sprite.hpp"
 
 namespace LuaSTGPlus
 {
-	// 动画资源
-	class ResAnimation : public ResourceBase
+	struct IResourceAnimation : public IResourceBase
 	{
-	private:
-		std::vector<Core::ScopeObject<Core::Graphics::ISprite>> m_sprites;
-		uint32_t m_Interval = 1;
-		BlendMode m_BlendMode = BlendMode::MulAlpha;
-		double m_HalfSizeX = 0.;
-		double m_HalfSizeY = 0.;
-		bool m_bRectangle = false;
-	public:
-		size_t GetCount() const noexcept { return m_sprites.size(); }
-		Core::Graphics::ISprite* GetSprite(uint32_t index);
-		uint32_t GetSpriteIndexByTimer(int ani_timer);
-		Core::Graphics::ISprite* GetSpriteByTimer(int ani_timer);
-		uint32_t GetInterval() const noexcept { return m_Interval; }
-		BlendMode GetBlendMode() const noexcept { return m_BlendMode; }
-		void SetBlendMode(BlendMode m) noexcept { m_BlendMode = m; }
-		double GetHalfSizeX() const noexcept { return m_HalfSizeX; }
-		double GetHalfSizeY() const noexcept { return m_HalfSizeY; }
-		bool IsRectangle() const noexcept { return m_bRectangle; }
-	public:
-		ResAnimation(const char* name, fcyRefPointer<ResTexture> tex,
-			float x, float y, float w, float h,
-			int n, int m, int intv,
-			double a, double b, bool rect = false);
-		~ResAnimation();
+		virtual size_t GetCount() = 0;
+		virtual Core::Graphics::ISprite* GetSprite(uint32_t index) = 0;
+		virtual uint32_t GetSpriteIndexByTimer(int ani_timer) = 0;
+		virtual Core::Graphics::ISprite* GetSpriteByTimer(int ani_timer) = 0;
+		virtual uint32_t GetInterval() = 0;
+		virtual BlendMode GetBlendMode() = 0;
+		virtual void SetBlendMode(BlendMode m) = 0;
+		virtual double GetHalfSizeX() = 0;
+		virtual double GetHalfSizeY() = 0;
+		virtual bool IsRectangle() = 0;
 	};
 }
