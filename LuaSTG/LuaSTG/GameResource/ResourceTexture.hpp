63710a0f1ea73b1562ad8739c3bb697eceabe3bd
璀境石 <Kuanlan@outlook.com>
2023-01-22 11:31:39 +0800

    迁移 ResourceTexture

--- LuaSTG/LuaSTG/GameResource/ResourceTexture.hpp
+++ LuaSTG/LuaSTG/GameResource/ResourceTexture.hpp
@@ -1,36 +1,17 @@
 ﻿#pragma once
 #include "GameResource/ResourceBase.hpp"
 #include "Core/Graphics/Device.hpp"
-#include "Core/Graphics/SwapChain.hpp"
 
 namespace LuaSTGPlus
 {
-	// 纹理资源
-	class ResTexture
-		: public ResourceBase
+	struct IResourceTexture : public IResourceBase
 	{
-	private:
-		Core::ScopeObject<Core::Graphics::ITexture2D> m_texture;
-		Core::ScopeObject<Core::Graphics::IRenderTarget> m_rt;
-		Core::ScopeObject<Core::Graphics::IDepthStencilBuffer> m_ds;
-		bool m_is_rendertarget{ false };
-		bool m_is_auto_resize{ false };
-		bool m_enable_depthbuffer{ false };
-	public:
-		bool ResizeRenderTarget(Core::Vector2U size);
-	public:
-		Core::Graphics::ITexture2D* GetTexture() { return m_texture.get(); }
-		Core::Graphics::IRenderTarget* GetRenderTarget() { return m_rt.get(); }
-		Core::Graphics::IDepthStencilBuffer* GetDepthStencilBuffer() { return m_ds.get(); }
-		bool IsRenderTarget() { return m_is_rendertarget; }
-		bool HasDepthStencilBuffer() { return m_enable_depthbuffer; }
-	public:
-		// 纹理容器
-		ResTexture(const char* name, Core::Graphics::ITexture2D* p_texture);
-		// 渲染附件容器
-		ResTexture(const char* name, int w, int h, bool ds);
-		// 自动调整大小的渲染附件容器
-		ResTexture(const char* name, bool ds);
-		~ResTexture();
+		virtual bool ResizeRenderTarget(Core::Vector2U size) = 0;
+
+		virtual Core::Graphics::ITexture2D* GetTexture() = 0;
+		virtual Core::Graphics::IRenderTarget* GetRenderTarget() = 0;
+		virtual Core::Graphics::IDepthStencilBuffer* GetDepthStencilBuffer() = 0;
+		virtual bool IsRenderTarget() = 0;
+		virtual bool HasDepthStencilBuffer() = 0;
 	};
 };
