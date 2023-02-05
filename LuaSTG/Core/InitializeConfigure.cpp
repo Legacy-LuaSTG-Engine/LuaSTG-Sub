db8aa890251de9d0d67802f0c7d4e573d2b02dd6
璀境石 <Kuanlan@outlook.com>
2023-02-05 17:22:59 +0800

    初始化配置文件

--- LuaSTG/Core/InitializeConfigure.cpp
+++ LuaSTG/Core/InitializeConfigure.cpp
@@ -6,28 +6,70 @@
 
 namespace Core
 {
+    void to_json(nlohmann::json& j, InitializeConfigure const& p)
+    {
+        j["target_graphics_device"] = p.target_graphics_device;
+
+        j["canvas_width"] = p.canvas_width;
+        j["canvas_height"] = p.canvas_height;
+
+        j["target_window_width"] = p.target_window_width;
+        j["target_window_height"] = p.target_window_height;
+        j["fullscreen_enable"] = p.fullscreen_enable;
+        j["vsync_enable"] = p.vsync_enable;
+        j["window_title"] = p.window_title;
+        j["window_cursor_enable"] = p.window_cursor_enable;
+
+        j["target_frame_rate"] = p.target_frame_rate;
+
+        j["music_channel_volume"] = p.music_channel_volume;
+        j["sound_effect_channel_volume"] = p.sound_effect_channel_volume;
+    }
+    void from_json(nlohmann::json const& j, InitializeConfigure& p)
+    {
+        j.at("target_graphics_device").get_to(p.target_graphics_device);
+
+        j.at("canvas_width").get_to(p.canvas_width);
+        j.at("canvas_height").get_to(p.canvas_height);
+
+        j.at("target_window_width").get_to(p.target_window_width);
+        j.at("target_window_height").get_to(p.target_window_height);
+        j.at("fullscreen_enable").get_to(p.fullscreen_enable);
+        j.at("vsync_enable").get_to(p.vsync_enable);
+        j.at("window_title").get_to(p.window_title);
+        j.at("window_cursor_enable").get_to(p.window_cursor_enable);
+
+        j.at("target_frame_rate").get_to(p.target_frame_rate);
+
+        j.at("music_channel_volume").get_to(p.music_channel_volume);
+        j.at("sound_effect_channel_volume").get_to(p.sound_effect_channel_volume);
+    }
+
     void InitializeConfigure::reset()
     {
-        gpu.clear();
-        width = 640;
-        height = 480;
-        refresh_rate_numerator = 0;
-        refresh_rate_denominator = 0;
-        windowed = true;
-        vsync = false;
+        target_graphics_device.clear();
+
+        canvas_width = 640;
+        canvas_height = 480;
+
+        target_window_width = 640;
+        target_window_height = 480;
+        fullscreen_enable = false;
+        vsync_enable = false;
+        window_title.clear();
+        window_cursor_enable = true;
+
+        target_frame_rate = 60;
+
+        music_channel_volume = 1.0f;
+        sound_effect_channel_volume = 1.0f;
     }
     bool InitializeConfigure::load(std::string_view const& source) noexcept
     {
         try
         {
             nlohmann::json json = nlohmann::json::parse(source);
-            gpu = json["gpu"].get<std::string>();
-            width = json["width"].get<int>();
-            height = json["height"].get<int>();
-            refresh_rate_numerator = json["refresh_rate_numerator"].get<int>();
-            refresh_rate_denominator = json["refresh_rate_denominator"].get<int>();
-            windowed = json["windowed"].get<bool>();
-            vsync = json["vsync"].get<bool>();
+            from_json(json, *this);
             return true;
         }
         catch (...)
@@ -41,14 +83,8 @@ namespace Core
         try
         {
             nlohmann::json json;
-            json["gpu"] = gpu;
-            json["width"] = width;
-            json["height"] = height;
-            json["refresh_rate_numerator"] = refresh_rate_numerator;
-            json["refresh_rate_denominator"] = refresh_rate_denominator;
-            json["windowed"] = windowed;
-            json["vsync"] = vsync;
-            buffer = std::move(json.dump());
+            to_json(json, *this);
+            buffer = std::move(json.dump(2));
             return true;
         }
         catch (...)
@@ -72,13 +108,7 @@ namespace Core
             }
             nlohmann::json json;
             file >> json;
-            gpu = json["gpu"].get<std::string>();
-            width = json["width"].get<int>();
-            height = json["height"].get<int>();
-            refresh_rate_numerator = json["refresh_rate_numerator"].get<int>();
-            refresh_rate_denominator = json["refresh_rate_denominator"].get<int>();
-            windowed = json["windowed"].get<bool>();
-            vsync = json["vsync"].get<bool>();
+            from_json(json, *this);
             return true;
         }
         catch (...)
@@ -92,13 +122,7 @@ namespace Core
         try
         {
             nlohmann::json json;
-            json["gpu"] = gpu;
-            json["width"] = width;
-            json["height"] = height;
-            json["refresh_rate_numerator"] = refresh_rate_numerator;
-            json["refresh_rate_denominator"] = refresh_rate_denominator;
-            json["windowed"] = windowed;
-            json["vsync"] = vsync;
+            to_json(json, *this);
             std::wstring wpath(utility::encoding::to_wide(path));
             std::ofstream file(wpath, std::ios::out | std::ios::binary | std::ios::trunc);
             if (!file.is_open())
