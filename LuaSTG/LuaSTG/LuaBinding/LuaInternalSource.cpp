9340338fed0bd837bf0fc7854c9b2edb8f7b09ce
璀境石 <Kuanlan@outlook.com>
2023-02-14 19:18:14 +0800

    角度值三角函数实现挪到lua里，让luajit优化

--- LuaSTG/LuaSTG/LuaBinding/LuaInternalSource.cpp
+++ LuaSTG/LuaSTG/LuaBinding/LuaInternalSource.cpp
@@ -79,6 +79,23 @@ if cjson then
     package.loaded["cjson"] = cjson -- fuck you cjson
 end
 
+local rad = math.rad
+local deg = math.deg
+local sin = math.sin
+local cos = math.cos
+local tan = math.tan
+local asin = math.asin
+local acos = math.acos
+local atan = math.atan
+local atan2 = math.atan2 or math.atan
+function lstg.sin(x) return sin(rad(x)) end
+function lstg.cos(x) return cos(rad(x)) end
+function lstg.tan(x) return tan(rad(x)) end
+function lstg.asin(x) return deg(asin(x)) end
+function lstg.acos(x) return deg(acos(x)) end
+function lstg.atan(...) return deg(atan(...)) end
+function lstg.atan2(y, x) return deg(atan2(y, x)) end
+
 )";
 #pragma endregion
 
