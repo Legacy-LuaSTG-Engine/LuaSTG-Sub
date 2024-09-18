3b05e56c9497e5fce9be818806aecb633af782f8
璀境石 <git@chuikingshek.com>
2024-09-18 19:54:57 +0800

    fxxk wil nuget package

--- cmake/packages.nuget.cmake
+++ cmake/packages.nuget.cmake
@@ -36,9 +36,11 @@ endif()
 
 CPMAddPackage(
     NAME Microsoft.Windows.ImplementationLibrary
-    VERSION 1.0.231216.1
-    URL https://www.nuget.org/api/v2/package/Microsoft.Windows.ImplementationLibrary/1.0.231216.1
-    URL_HASH SHA256=5557B5C82D00DB3BD539AA886058475BE552EE09B538CEF87CBCAFF440BAF5E8
+    VERSION 1.0.240803.1
+    GITHUB_REPOSITORY microsoft/wil
+    # 你妈的解压出来的的路径这么长，动不动就超出 Windows 路径长度限制
+    #URL https://www.nuget.org/api/v2/package/Microsoft.Windows.ImplementationLibrary/1.0.231216.1
+    #URL_HASH SHA256=5557B5C82D00DB3BD539AA886058475BE552EE09B538CEF87CBCAFF440BAF5E8
     DOWNLOAD_ONLY YES
 )
 
