cd09cfb657cc3383d87a938741f7cb19af50f95e
璀境石 <git@chuikingshek.com>
2025-02-24 01:34:34 +0800

    add override modifier

--- LuaSTG/Core/Graphics/Common/TextRenderer.hpp
+++ LuaSTG/Core/Graphics/Common/TextRenderer.hpp
@@ -10,21 +10,22 @@ namespace Core::Graphics::Common
 	public:
 		// ITextRenderer
 
-		void setScale(Vector2F const& scale) { m_scale = scale; }
-		Vector2F getScale() { return m_scale; }
-		void setColor(Color4B const color) { m_color = color; }
-		Color4B getColor() { return m_color; }
-		void setZ(float const z) { m_z = z; }
-		float getZ() { return m_z; }
-		void setGlyphManager(IGlyphManager* p_mgr) { m_glyph_mgr = p_mgr; }
-		IGlyphManager* getGlyphManager() { return m_glyph_mgr.get(); }
+		void setScale(Vector2F const& scale) override { m_scale = scale; }
+		Vector2F getScale() override { return m_scale; }
+		void setColor(Color4B const color) override { m_color = color; }
+		Color4B getColor() override { return m_color; }
+		void setZ(float const z) override { m_z = z; }
+		float getZ() override { return m_z; }
+		void setGlyphManager(IGlyphManager* p_mgr) override { m_glyph_mgr = p_mgr; }
+		IGlyphManager* getGlyphManager() override { return m_glyph_mgr.get(); }
 
-		RectF getTextBoundary(StringView str);
-		Vector2F getTextAdvance(StringView str);
-		bool drawText(StringView str, Vector2F const& start, Vector2F* endout);
-		bool drawTextInSpace(StringView str,
+		RectF getTextBoundary(StringView str) override;
+		Vector2F getTextAdvance(StringView str) override;
+		bool drawText(StringView str, Vector2F const& start, Vector2F* endout) override;
+		bool drawTextInSpace(
+			StringView str,
 			Vector3F const& start, Vector3F const& right_vec, Vector3F const& down_vec,
-			Vector3F* endout);
+			Vector3F* endout) override;
 
 		// TextRenderer
 
