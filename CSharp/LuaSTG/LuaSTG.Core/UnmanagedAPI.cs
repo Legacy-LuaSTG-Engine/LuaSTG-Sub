71ae860d967d1180bc58e0f70d82151d56cd9938
czh098tom <czh098tom@163.com>
2025-03-30 14:51:02 +0800

    try marshal string

--- CSharp/LuaSTG/LuaSTG.Core/UnmanagedAPI.cs
+++ CSharp/LuaSTG/LuaSTG.Core/UnmanagedAPI.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace LuaSTG.Core
     [StructLayout(LayoutKind.Sequential)]
     internal unsafe struct UnmanagedAPI
     {
+        public delegate* unmanaged<int, IntPtr, void> log;
         public delegate* unmanaged<void> beginScene;
         public delegate* unmanaged<void> endScene;
         public delegate* unmanaged<byte, byte, byte, byte, void> renderClear;
@@ -17,8 +19,17 @@ namespace LuaSTG.Core
 
     public static unsafe partial class LuaSTGAPI
     {
+        public static void Log(LogLevel level, string message)
+        {
+            IntPtr unmanagedString = Marshal.StringToHGlobalAnsi(message);
+            api.log((int) level, unmanagedString);
+            Marshal.FreeHGlobal(unmanagedString);
+        }
+
         public static void BeginScene() => api.beginScene();
+
         public static void EndScene() => api.endScene();
+
         public static void RenderClear(byte a, byte r, byte g, byte b) => api.renderClear(a, r, g, b);
 
         private static UnmanagedAPI api;
--- LuaSTG/LuaSTG/CLRBinding/CLRBinding.hpp
+++ LuaSTG/LuaSTG/CLRBinding/CLRBinding.hpp
@@ -2,26 +2,27 @@
 
 #include "CLRBinding/CLRHost.hpp"
 
-#define CORECLR_BINDING(ret, name, parameter) typedef ret (CORECLR_DELEGATE_CALLTYPE* t_##name)(parameter);\
-t_##name name;
-
 namespace LuaSTGPlus
 {
 	struct CLRInitPayload
 	{
+		void (CORECLR_DELEGATE_CALLTYPE* Log)(int32_t, intptr_t);
+
 		void (CORECLR_DELEGATE_CALLTYPE* BeginScene)();
 		void (CORECLR_DELEGATE_CALLTYPE* EndScene)();
-		void (CORECLR_DELEGATE_CALLTYPE* RenderClear)(unsigned char, unsigned char, unsigned char, unsigned char);
+		void (CORECLR_DELEGATE_CALLTYPE* RenderClear)(uint8_t, uint8_t, uint8_t, uint8_t);
 	};
 
 	struct CLRFunctions 
 	{
+#pragma region GameLoop
 		void (CORECLR_DELEGATE_CALLTYPE* GameInit)();
 		bool (CORECLR_DELEGATE_CALLTYPE* FrameFunc)();
 		void (CORECLR_DELEGATE_CALLTYPE* RenderFunc)();
 		void (CORECLR_DELEGATE_CALLTYPE* GameExit)();
 		void (CORECLR_DELEGATE_CALLTYPE* FocusLoseFunc)();
 		void (CORECLR_DELEGATE_CALLTYPE* FocusGainFunc)();
+#pragma endregion
 	};
 
 	typedef int (CORECLR_DELEGATE_CALLTYPE* entry_point_fn)(CLRInitPayload*, CLRFunctions*);
