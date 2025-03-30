60521341892d8855f807e8324e54ceefda6e9f50
czh098tom <czh098tom@163.com>
2025-03-30 15:52:02 +0800

    add comments

--- CSharp/LuaSTG/LuaSTG.Core/UnmanagedAPI.cs
+++ CSharp/LuaSTG/LuaSTG.Core/UnmanagedAPI.cs
@@ -19,6 +19,11 @@ namespace LuaSTG.Core
 
     public static unsafe partial class LuaSTGAPI
     {
+        /// <summary>
+        /// Print a log to LuaSTG Engine.
+        /// </summary>
+        /// <param name="level">Logging level.</param>
+        /// <param name="message">Logging message.</param>
         public static void Log(LogLevel level, string message)
         {
             IntPtr unmanagedString = Marshal.StringToHGlobalAnsi(message);
@@ -26,10 +31,23 @@ namespace LuaSTG.Core
             Marshal.FreeHGlobal(unmanagedString);
         }
 
+        /// <summary>
+        /// Notify engine to start rendering.
+        /// </summary>
         public static void BeginScene() => api.beginScene();
 
+        /// <summary>
+        /// Notify engine to finish rendering.
+        /// </summary>
         public static void EndScene() => api.endScene();
 
+        /// <summary>
+        /// Clear the current render target by the color provided.
+        /// </summary>
+        /// <param name="a">Alpha value.</param>
+        /// <param name="r">Red value.</param>
+        /// <param name="g">Green value.</param>
+        /// <param name="b">Blue value.</param>
         public static void RenderClear(byte a, byte r, byte g, byte b) => api.renderClear(a, r, g, b);
 
         private static UnmanagedAPI api;
