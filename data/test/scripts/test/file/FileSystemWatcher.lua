85fa844374c43a2d6edb0451a4776e4f69ff2463
璀境石 <git@chuikingshek.com>
2025-04-30 19:46:37 +0800

    test

--- data/test/scripts/test/file/FileSystemWatcher.lua
+++ data/test/scripts/test/file/FileSystemWatcher.lua
@@ -1,6 +1,7 @@
 local test = require("test")
 local imgui = require("imgui")
 local FileSystemWatcher = require("lstg.FileSystemWatcher")
+local FileAction = require("lstg.FileSystemWatcher.FileAction")
 
 local TEST_NAME = "File: FileSystemWatcher"
 
@@ -13,6 +14,12 @@ end
 local M = {}
 
 function M:onCreate()
+    refreshGlyphCache("添加")
+    refreshGlyphCache("移除")
+    refreshGlyphCache("修改")
+    refreshGlyphCache("重命名（旧）")
+    refreshGlyphCache("重命名（新）")
+    refreshGlyphCache("未知")
     self.watcher = FileSystemWatcher.create("C:\\Users\\Admin\\Downloads");
     self.changes = {}
 end
@@ -21,18 +28,28 @@ function M:onDestroy()
     self.watcher:close()
 end
 
+local function translateFileAction(action)
+    if FileAction.added == action then return "添加" end
+    if FileAction.removed == action then return "移除" end
+    if FileAction.modified == action then return "修改" end
+    if FileAction.renamed_old_name == action then return "重命名（旧）" end
+    if FileAction.renamed_new_name == action then return "重命名（新）" end
+    return "未知"
+end
+
 function M:onUpdate()
     local change = { file_name = "", action = 0 }
     while self.watcher:read(change) do
         table.insert(self.changes, { file_name = change.file_name, action = change.action })
         refreshGlyphCache(change.file_name)
     end
-
+    
+    
     ---@diagnostic disable-next-line: undefined-field
     local ImGui = imgui.ImGui
     if ImGui.Begin(TEST_NAME) then
         for i, v in ipairs(self.changes) do
-            ImGui.Text(string.format("%d. (%d) %s", i, v.action, v.file_name))
+            ImGui.Text(string.format("%d. %s %s", i, translateFileAction(v.action), v.file_name))
         end
     end
     ImGui.End()
