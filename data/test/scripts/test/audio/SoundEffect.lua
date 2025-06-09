dcb20c696199545a9ce59bb966c5db3377778d6d
璀境石 <git@chuikingshek.com>
2025-06-10 00:10:27 +0800

    se pan test

--- data/test/scripts/test/audio/SoundEffect.lua
+++ data/test/scripts/test/audio/SoundEffect.lua
@@ -1,36 +1,42 @@
 local test = require("test")
 local imgui = require("imgui")
 
+local SE_NAME = "啊！"
+
 ---@class test.audio.SoundEffect : test.Base
 local M = {}
 
 function M:onCreate()
     local old = lstg.GetResourceStatus()
     lstg.SetResourceStatus("global")
-    lstg.LoadSound("啊！", "res/audio/啊！.wav")
+    lstg.LoadSound(SE_NAME, "res/audio/啊！.wav")
     lstg.SetResourceStatus(old)
+    self.vol = 1.0
+    self.pan = 0.0
 end
 
 function M:onDestroy()
-    lstg.RemoveResource("global", 5, "啊！")
+    lstg.RemoveResource("global", 5, SE_NAME)
 end
 
 function M:onUpdate()
     ---@diagnostic disable-next-line: undefined-field
     local ImGui = imgui.ImGui
     if ImGui.Begin("Audio: Sound Effect") then
-        ImGui.Text(("SE State: %s"):format(lstg.GetSoundState("啊！")))
+        ImGui.Text(("SE State: %s"):format(lstg.GetSoundState(SE_NAME)))
+        _, self.vol = ImGui.SliderFloat("Volume", self.vol, 0.0, 1.0)
+        _, self.pan = ImGui.SliderFloat("Pan", self.pan, -1.0, 1.0)
         if ImGui.Button("Play") then
-            lstg.PlaySound("啊！")
+            lstg.PlaySound(SE_NAME, self.vol, self.pan)
         end
         if ImGui.Button("Pause") then
-            lstg.PauseSound("啊！")
+            lstg.PauseSound(SE_NAME)
         end
         if ImGui.Button("Resume") then
-            lstg.ResumeSound("啊！")
+            lstg.ResumeSound(SE_NAME)
         end
         if ImGui.Button("Stop") then
-            lstg.StopSound("啊！")
+            lstg.StopSound(SE_NAME)
         end
     end
     ImGui.End()
