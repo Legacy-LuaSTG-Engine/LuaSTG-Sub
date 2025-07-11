0629dcd1ac7606af19021c22ef98dae0855a5fac
璀境石 <git@chuikingshek.com>
2025-07-11 19:42:18 +0800

    better imgui binding: GetStyleColorU32, stupid GetColorU32!!!

--- imgui/binding/modern/part/ImGui-13-StyleReadAccess.cpp
+++ imgui/binding/modern/part/ImGui-13-StyleReadAccess.cpp
@@ -12,24 +12,24 @@ namespace {
 	int GetColorU32(lua_State* const vm) {
 		lua::stack_t const ctx(vm);
 		if (ctx.is_number(1)) {
-			auto const idx = ctx.get_value<ImGuiCol>(1);
 			auto const col = ctx.get_value<ImU32>(1);
 			auto const alpha_mul = ctx.get_value<float>(2, 1.0f);
-			// MAGIC CODE!!!
-			if (idx >= ImGuiCol_Text && idx < ImGuiCol_COUNT) {
-				auto const result = ImGui::GetColorU32(idx, alpha_mul);
-				ctx.push_value(result);
-			}
-			else {
-				auto const result = ImGui::GetColorU32(col, alpha_mul);
-				ctx.push_value(result);
-			}
+			auto const result = ImGui::GetColorU32(col, alpha_mul);
+			ctx.push_value(result);
 		}
 		else {
 			auto const col = imgui::binding::ImVec4Binding::as(vm, 1);
 			auto const result = ImGui::GetColorU32(*col);
 			ctx.push_value(result);
 		}
+		return 1;
+	}
+	int GetStyleColorU32(lua_State* const vm) {
+		lua::stack_t const ctx(vm);
+		auto const idx = ctx.get_value<ImGuiCol>(1);
+		auto const alpha_mul = ctx.get_value<float>(2, 1.0f);
+		auto const result = ImGui::GetColorU32(idx, alpha_mul);
+		ctx.push_value(result);
 		return 1;
 	}
 	int GetStyleColorVec4(lua_State* const vm) {
@@ -49,6 +49,7 @@ namespace imgui::binding {
 		auto const m = ctx.push_module(module_ImGui_name);
 		ctx.set_map_value(m, "GetFontTexUvWhitePixel"sv, &GetFontTexUvWhitePixel);
 		ctx.set_map_value(m, "GetColorU32"sv, &GetColorU32);
+		ctx.set_map_value(m, "GetStyleColorU32"sv, &GetStyleColorU32);
 		ctx.set_map_value(m, "GetStyleColorVec4"sv, &GetStyleColorVec4);
 	}
 }
