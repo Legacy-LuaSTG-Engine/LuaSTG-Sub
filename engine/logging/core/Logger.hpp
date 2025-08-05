94baaf3ac13e83a850b132af959083ee85be50c3
璀境石 <git@chuikingshek.com>
2025-08-05 11:34:27 +0800

    c++ 20 P2508R1

--- engine/logging/core/Logger.hpp
+++ engine/logging/core/Logger.hpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <format>
 
+// VS 2022 17.5: P2508R1 basic_format_string, format_string, wformat_string
+
 namespace core {
 	class Logger {
 	public:
@@ -14,10 +16,17 @@ namespace core {
 			info(trim({ message, Size }));
 		}
 
+	#if _MSC_VER >= 1935 // Visual Studio 2022 version 17.5
 		template <class... Types>
 		static void info(const std::format_string<Types...> fmt, Types&&... args) {
 			info(std::vformat(fmt.get(), std::make_format_args(args...)));
 		}
+	#else
+		template <class... Types>
+		static void info(std::string_view const fmt, Types&&... args) {
+			info(std::vformat(fmt, std::make_format_args(args...)));
+		}
+	#endif
 
 		static void warn(std::string_view const& message);
 		static void warn(std::string const& message);
@@ -27,10 +36,17 @@ namespace core {
 			warn(trim({ message, Size }));
 		}
 
+	#if _MSC_VER >= 1935 // Visual Studio 2022 version 17.5
 		template <class... Types>
 		static void warn(const std::format_string<Types...> fmt, Types&&... args) {
 			warn(std::vformat(fmt.get(), std::make_format_args(args...)));
 		}
+	#else
+		template <class... Types>
+		static void warn(std::string_view const fmt, Types&&... args) {
+			warn(std::vformat(fmt, std::make_format_args(args...)));
+		}
+	#endif
 
 		static void error(std::string_view const& message);
 		static void error(std::string const& message);
@@ -40,10 +56,17 @@ namespace core {
 			error(trim({ message, Size }));
 		}
 
+	#if _MSC_VER >= 1935 // Visual Studio 2022 version 17.5
 		template <class... Types>
 		static void error(const std::format_string<Types...> fmt, Types&&... args) {
 			error(std::vformat(fmt.get(), std::make_format_args(args...)));
 		}
+	#else
+		template <class... Types>
+		static void error(std::string_view const fmt, Types&&... args) {
+			error(std::vformat(fmt, std::make_format_args(args...)));
+		}
+	#endif
 
 	private:
 		static std::string_view trim(std::string_view const& message) {
