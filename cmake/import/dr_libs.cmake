d329041b87d44c6274436bbc61a2bb4bf1e61905
璀境石 <git@chuikingshek.com>
2026-02-02 10:39:30 +0800

    dependencies: dr_flac v0.13.3, dr_mp3 v0.7.3, dr_wav v0.14.4

--- cmake/import/dr_libs.cmake
+++ cmake/import/dr_libs.cmake
@@ -2,9 +2,9 @@
 
 CPMAddPackage(
     NAME dr_libs
-    VERSION 2025.9.10
-    URL https://github.com/mackron/dr_libs/archive/3141b54b6b0067d15c4a3ec0877f2141a2a11347.zip
-    URL_HASH SHA256=4B5050ED55C598311ED34FB8E59D8D9E356F093C2603ED3599807FA4112475C2
+    VERSION 2026.1.17
+    URL https://github.com/mackron/dr_libs/archive/5690d4671d7ad07ae6021756d7222eb159745f06.zip
+    URL_HASH SHA256=DB9C1738C1D90EE81C0D04B35D0EA09661F1FCE00F321FD1159655819B0B8E85
     DOWNLOAD_ONLY YES
 )
 
@@ -13,21 +13,11 @@ if(dr_libs_ADDED)
     target_include_directories(dr_libs PUBLIC
         ${dr_libs_SOURCE_DIR}
     )
-    if (NOT EXISTS ${CMAKE_BINARY_DIR}/dr_libs/dr_libs.c)
-        file(WRITE ${CMAKE_BINARY_DIR}/dr_libs/dr_libs.c
-            "#define DR_WAV_IMPLEMENTATION\n"
-            "#define DR_MP3_IMPLEMENTATION\n"
-            "#define DR_FLAC_IMPLEMENTATION\n"
-            "#include \"dr_wav.h\"\n"
-            "#include \"dr_mp3.h\"\n"
-            "#include \"dr_flac.h\"\n"
-        )
-    endif ()
     target_sources(dr_libs PRIVATE
         ${dr_libs_SOURCE_DIR}/dr_wav.h
         ${dr_libs_SOURCE_DIR}/dr_mp3.h
         ${dr_libs_SOURCE_DIR}/dr_flac.h
-        ${CMAKE_BINARY_DIR}/dr_libs/dr_libs.c
+        ${CMAKE_CURRENT_LIST_DIR}/dr_libs/dr_libs.c
     )
     set_target_properties(dr_libs PROPERTIES FOLDER external)
 endif()
