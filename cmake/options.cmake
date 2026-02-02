278c69d1b33b571f86f3e3a9cdcef6a8142c4b2b
璀境石 <git@chuikingshek.com>
2026-02-02 14:28:27 +0800

    cmake: remove useless cmake file

--- cmake/options.cmake
+++
@@ -1,6 +0,0 @@
-# utf-8 source files and binary 
-
-add_library(options_compile_utf8 INTERFACE)
-if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    target_compile_options(options_compile_utf8 INTERFACE "/utf-8")
-endif ()
