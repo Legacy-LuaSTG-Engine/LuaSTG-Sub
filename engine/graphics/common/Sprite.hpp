f4b0fa346595f26b808ee547dc6678bda3272252
璀境石 <git@chuikingshek.com>
2026-02-23 22:27:27 +0800

    dev: Sprite code review

--- engine/graphics/common/Sprite.hpp
+++ engine/graphics/common/Sprite.hpp
@@ -9,24 +9,24 @@ namespace core {
         // ISprite
 
         ITexture2D* getTexture() override { return m_texture.get(); }
-        void setTexture(ITexture2D* const p_texture) override { m_texture = p_texture; }
-        void setTextureRect(RectF const& rc) override;
+        void setTexture(ITexture2D* const texture) override { m_texture = texture; }
+        void setTextureRect(const RectF& rc) override;
         RectF getTextureRect() override { return m_rect; }
-        void setTextureCenter(Vector2F const& pt) override;
+        void setTextureCenter(const Vector2F& pt) override;
         Vector2F getTextureCenter() override { return m_center; }
         void setUnitsPerPixel(float v) override;
         float getUnitsPerPixel() override { return m_scale; }
 
-        bool clone(ISprite** pp_sprite) override;
+        bool clone(ISprite** out_sprite) override;
 
         // Sprite
 
-        Sprite(ITexture2D* p_texture);
-        Sprite(Sprite const&) = delete;
+        Sprite(ITexture2D* texture);
+        Sprite(const Sprite&) = delete;
         Sprite(Sprite&&) = delete;
         ~Sprite();
 
-        Sprite& operator=(Sprite const&) = delete;
+        Sprite& operator=(const Sprite&) = delete;
         Sprite& operator=(Sprite&&) = delete;
 
     private:
